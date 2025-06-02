953c3976b1a839bac745c3266334a5df82763bd3
davidpark <tlsdnd001@naver.com>
2025-06-03 01:09:07 +0900

    First Source Commit

---
+++ src/main/java/com/spring/recommend_contents/RecommendContentsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.recommend_contents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendContentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendContentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/recommend_contents/RecommendContentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.recommend_contents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendContentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

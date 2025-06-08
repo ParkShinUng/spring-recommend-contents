ac45d8fee2d15a65a9e2d83980773f8c93c7979b
davidpark <tlsdnd001@naver.com>
2025-06-08 16:44:20 +0900

    - 회원가입 기능 구현  : 회원 등록  : 이메일 인증 - 로그인 기능 구현 - Open AI API 연동  : 감정 기반 Prompt 요청 및 응답  : 응답 결과 파싱 - 콘텐츠 추천 리스트 출력 화면 구현

--- src/test/java/com/spring/recommend_contents/RecommendContentsApplicationTests.java
+++ src/test/java/com/spring/recommend_contents/RecommendContentsApplicationTests.java
@@ -1,13 +1,24 @@
 package com.spring.recommend_contents;
 
+import com.spring.recommend_contents.user.UserService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RecommendContentsApplicationTests {
 
+	@Autowired
+	private UserService userService;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Transactional
+	@Test
+	void testCreateUser() {
+		this.userService.create("tlsdnd001@gmail.com", "1234");
+	}
 }
